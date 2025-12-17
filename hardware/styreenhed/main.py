1545d692c9300988be471bc01df5ef210372ee02
YasoobMukhtar11 <yamu0002@stud.ek.dk>
2025-12-17 13:17:19 +0100

    Kommentar fjernet

--- hardware/styreenhed/main.py
+++ hardware/styreenhed/main.py
@@ -1,20 +1,18 @@
 import network
 import espnow
 
-# 1) WiFi i station mode (krævet for ESP-NOW)
 w0 = network.WLAN(network.STA_IF)
 w0.active(True)
 
 print("Receiver MAC:", w0.config('mac'))
 
-# 2) Start ESP-NOW
 e = espnow.ESPNow()
 e.active(True)
 
 print("ESP-NOW receiver klar... venter på beskeder")
 
 while True:
-    host, msg = e.recv()  # Blokkerer indtil der kommer noget
+    host, msg = e.recv()  
     if msg:
         try:
             text = msg.decode()
