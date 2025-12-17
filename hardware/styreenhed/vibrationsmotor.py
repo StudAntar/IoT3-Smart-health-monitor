e2a137354d60b3ad26128626da0d9f303eab308e
YasoobMukhtar11 <yamu0002@stud.ek.dk>
2025-12-17 13:17:39 +0100

    Kommentar fjernet

--- hardware/styreenhed/vibrationsmotor.py
+++ hardware/styreenhed/vibrationsmotor.py
@@ -1,17 +1,16 @@
 from machine import Pin
 import time
 
-motor = Pin(5, Pin.OUT)        # V919 IN-pin
-button = Pin(19, Pin.IN)       # HW-483 S-pin
+motor = Pin(5, Pin.OUT)        
+button = Pin(19, Pin.IN)       
 
 while True:
     state = button.value()
 
-    # HW-483 er normalt AKTIV LOW (0 = trykket)
     if state == 0:
         motor.value(1)
     else:
         motor.value(0)
 
-    time.sleep(0.01)  # Lidt pause for stabilitet
+    time.sleep(0.01)  
 
